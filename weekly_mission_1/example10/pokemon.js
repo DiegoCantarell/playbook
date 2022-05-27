825d5217ffcefb15d577d9a7eb0f2225fe1f1b73
Diego Cantarell <92350521+DiegoCantarell@users.noreply.github.com>
2022-05-27 16:15:16 -0500

    Update pokemon.js

--- weekly_mission_1/example10/pokemon.js
+++ weekly_mission_1/example10/pokemon.js
@@ -1,21 +1,4 @@
-/*|Example 9: Ejercicio.
-
-Indicaciones :
-
-Crea el archivo pokemon.js. 3, Crea una clase Pokemon, exportala usando CommonJS, de tal forma que pueda usarse como se indica previamente.
-
-Crea la clase.
-
-Crea un constructor que permita recibir un nombre.
-
-Crea la función sayHello().
-
-Crea la función sayMessage().
-
-Exporta la clase para que pueda ser importada en el archivo main.js.
-
-Este ejercicio es producto de todos los previos ejercicios.
-*/
+//|Example 10: Ejercicio.
 
 export default class Pokemon {
 	constructor (name){
