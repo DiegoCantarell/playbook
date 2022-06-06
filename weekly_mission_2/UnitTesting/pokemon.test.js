a04ae98240795dbd146274622ca060b1a59b8656
kantrax <diegogccantarell@gmail.com>
2022-06-06 07:30:06 -0500

    10  Adding  UnitTesting

--- weekly_mission_2/UnitTesting/pokemon.test.js
+++ weekly_mission_2/UnitTesting/pokemon.test.js
@@ -3,4 +3,7 @@ import Pokemon from './pokemon'
 test('1) Create a new object pokemon', () => {
   const myPokemon = new Pokemon('Pikachu')
   expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
+	//DESCARGA LAS DEPENDENCIAS DENTRO DE ESTE DIRECTORIO USANDO npm install
+	//ESE COMANDO VA A DESCARGAR TODAS LAS DEPENDENCIAS NECESARIAS
+	//PARA CORRER EL TEST ES NECESARIO TENER LAS DEPENDENCIAS Y SE CORRE LAS PRUEBAS USANDO npm test
 });
